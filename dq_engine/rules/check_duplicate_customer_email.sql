9de569f8057ec3efd699be30afc2420c8e12ae8c
Lizi Lingamaneni <lizivishnu@Lizis-MacBook-Air.local>
2026-03-11 15:22:03 -0400

    two commits merged in to one with rebase

--- dq_engine/rules/check_duplicate_customer_email.sql
+++ dq_engine/rules/check_duplicate_customer_email.sql
@@ -1,5 +1,5 @@
 -- Rule : Check for duplicate email addresses in the customers table
--- Purpiose: Ensure that there are no duplicate email addresses in the customers table, which is important for maintaining data integrity and avoiding issues with customer communication.
+-- Purpiose:   Ensure that there are no duplicate email addresses in the customers table, which is important for maintaining data integrity and avoiding issues with customer communication.
 
 Select customer_email, COUNT(*) AS duplicate_email_count
 FROM customers
